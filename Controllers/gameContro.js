99ab31ce999dafa24945fe927ec38338c9fad712
Shah Rukh Khan <164313837+iamsrk0786@users.noreply.github.com>
2025-06-11 12:01:17 +0530

    Update gameContro.js

--- Controllers/gameContro.js
+++ Controllers/gameContro.js
@@ -1,6 +1,6 @@
 import Game from '../models/game.js';
 
-const getGamesByDate = async (date, direction, limit = 30) => {
+const getGamesByDate = async (date, direction, limit = 30) => { // using loop version
   const inputDate = new Date(date);
 
   for (let i = 0; i <= limit; i++) {
@@ -28,6 +28,26 @@ const getGamesByDate = async (date, direction, limit = 30) => {
 
   return null;
 };
+
+//const getGamesByDate = async (date, direction, attempts = 0, limit = 30) => { // using Recursive version
+ // if (attempts > limit) return null;
+//const currentDate = new Date(date);
+ // currentDate.setDate(currentDate.getDate() + (direction === 'down' ? -attempts : attempts));
+//const startOfDay = new Date(currentDate);
+ // startOfDay.setHours(0, 0, 0, 0);
+//const endOfDay = new Date(currentDate);
+ // endOfDay.setHours(23, 59, 59, 999);
+//const games = await Game.find({
+    //date: { $gte: startOfDay, $lte: endOfDay },
+ // }).exec();
+//if (games.length > 0) {
+    //return games;
+ // }
+// ab next day ke liye function ko dubara call karo
+ // return getGamesByDate(date, direction, attempts + 1, limit);
+//};
+
+
 
 export const getGamesByDateHandler = async (req, res) => {
   try {
